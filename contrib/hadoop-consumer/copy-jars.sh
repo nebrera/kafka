3afc65ba194457137f0237078a01bd7cdd905c99
Hagen Rother <hagen.rother@madvertise.com>
2013-01-11 16:55:48 +0100

    Updated the copy-jars.sh script

--- contrib/hadoop-consumer/copy-jars.sh
+++ contrib/hadoop-consumer/copy-jars.sh
@@ -14,56 +14,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ $# -lt 1 ];
-then
-  echo "USAGE: $0 dir"
-  exit 1
-fi
+
+hdfs_dir="/tmp/kafka/lib"
 
 base_dir=$(dirname $0)/../..
 
-hadoop=${HADOOP_HOME}/bin/hadoop
+hadoop=`which hadoop`
 
-echo "$hadoop fs -rmr $1"
-$hadoop fs -rmr $1
+echo "$hadoop fs -rmr $hdfs_dir"
+$hadoop fs -rmr $hdfs_dir
 
-echo "$hadoop fs -mkdir $1"
-$hadoop fs -mkdir $1
+echo "$hadoop fs -mkdir $hdfs_dir"
+$hadoop fs -mkdir $hdfs_dir
 
 # include kafka jars
 for file in $base_dir/contrib/hadoop-consumer/target/scala_2.8.0/*.jar;
 do
-   echo "$hadoop fs -put $file $1/"
-   $hadoop fs -put $file $1/ 
+   echo "$hadoop fs -put $file $hdfs_dir/"
+   $hadoop fs -put $file $hdfs_dir/ 
 done
 
 # include kafka jars
-echo "$hadoop fs -put $base_dir/core/target/scala_2.8.0/kafka-*.jar; $1/"
-$hadoop fs -put $base_dir/core/target/scala_2.8.0/kafka-*.jar $1/ 
+echo "$hadoop fs -put $base_dir/core/target/scala_2.8.0/kafka-*.jar; $hdfs_dir/"
+$hadoop fs -put $base_dir/core/target/scala_2.8.0/kafka-*.jar $hdfs_dir/ 
 
 # include core lib jars
 for file in $base_dir/core/lib/*.jar;
 do
-   echo "$hadoop fs -put $file $1/"
-   $hadoop fs -put $file $1/ 
+   echo "$hadoop fs -put $file $hdfs_dir/"
+   $hadoop fs -put $file $hdfs_dir/ 
 done
 
 for file in $base_dir/core/lib_managed/scala_2.8.0/compile/*.jar;
 do
-   echo "$hadoop fs -put $file $1/"
-   $hadoop fs -put $file $1/ 
+   echo "$hadoop fs -put $file $hdfs_dir/"
+   $hadoop fs -put $file $hdfs_dir/ 
 done
 
 # include scala library jar
-echo "$hadoop fs -put $base_dir/project/boot/scala-2.8.0/lib/scala-library.jar; $1/"
-$hadoop fs -put $base_dir/project/boot/scala-2.8.0/lib/scala-library.jar $1/
+echo "$hadoop fs -put $base_dir/project/boot/scala-2.8.0/lib/scala-library.jar; $hdfs_dir/"
+$hadoop fs -put $base_dir/project/boot/scala-2.8.0/lib/scala-library.jar $hdfs_dir/
 
 local_dir=$(dirname $0)
 
 # include hadoop-consumer jars
 for file in $local_dir/lib/*.jar;
 do
-   echo "$hadoop fs -put $file $1/"
-   $hadoop fs -put $file $1/ 
+   echo "$hadoop fs -put $file $hdfs_dir/"
+   $hadoop fs -put $file $hdfs_dir/ 
 done
 
