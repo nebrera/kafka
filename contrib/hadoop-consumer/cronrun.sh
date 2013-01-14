6f05fb3df2c50c9de9290920c8795083c8e2d59e
Hostmaster of the day <root@localhost>
2013-01-14 15:40:51 +0100

    Add a cronrun.sh script for cron-based hadoop import

---
+++ contrib/hadoop-consumer/cronrun.sh
@@ -0,0 +1,9 @@
+PROGRAM=`dirname $0`/hadoop-importer.sh
+LOG_FACILITY="local1"
+
+PATH=/bin:/usr/bin:/opt/hadoop/bin
+
+exec 1> >(logger -i -p "${LOG_FACILITY}.info" -t "${PROGRAM}")
+exec 2> >(logger -i -p "${LOG_FACILITY}.error" -t "${PROGRAM}")
+
+$PROGRAM
