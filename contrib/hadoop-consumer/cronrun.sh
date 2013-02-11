53b08eb70f7b4d2ab369e035994d9ee8b952e417
Hostmaster of the day <root@localhost>
2013-02-11 15:57:50 +0100

    Added a check if a job is already running
    iThis avoid double runs on very high loads in hadoop

--- contrib/hadoop-consumer/cronrun.sh
+++ contrib/hadoop-consumer/cronrun.sh
@@ -4,7 +4,17 @@ LOG_FACILITY="local1"
 
 PATH=/bin:/usr/bin:/opt/hadoop/bin
 
-exec 1> >(logger -i -p "${LOG_FACILITY}.info" -t "${PROGRAM}")
-exec 2> >(logger -i -p "${LOG_FACILITY}.error" -t "${PROGRAM}")
+#exec 1> >(logger -i -p "${LOG_FACILITY}.info" -t "${PROGRAM}")
+#exec 2> >(logger -i -p "${LOG_FACILITY}.error" -t "${PROGRAM}")
 
-$PROGRAM
+echo Checking for existing job
+
+hadoop job -list 2>/dev/null|grep `whoami`  
+
+if [[ $? -ne 0 ]]; then
+  echo Starting $PROGRAM
+  $PROGRAM
+else
+  echo "Not starting import, still running"
+fi
+
